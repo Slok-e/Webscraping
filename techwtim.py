da5040df35fbf7320949d3c8a1c57d74e16fc016
Slok-e <106457601+Slok-e@users.noreply.github.com>
2022-12-22 16:49:40 -0500

    update

--- techwtim.py
+++ techwtim.py
@@ -1,7 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # I reccomend changing the web driver location to your program(x86)folder (Windows) or /Library/ (Mac) or somewhere you can easily allocate.
 PATH = "/Library/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.techwithtim.net")
+print(driver.title)
+
+# Input to search box on page.
+search = driver.find_element(By.NAME, "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    print(main.text)
+finally:
+    driver.quit()
